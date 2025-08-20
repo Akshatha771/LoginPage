ee81f2d6f316eecb9fa22d1369957496a4adac4a
Akshatha <akshathabb77@gmail.com>
2025-08-20 13:58:06 +0530

    Added all files

---
+++ script.js
@@ -0,0 +1,14 @@
+function validateLogin(){
+    var username= document.getElementById('username').value;
+    var password= document.getElementById('password').value;
+    if(username==='admin' && password == '1234'){
+        window.location.href='landing.html';
+        return false;
+    }else{
+        alert("Invalid username or password!");
+        return false;
+    }
+
+
+
+}
